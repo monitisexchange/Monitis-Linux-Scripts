f20feef3eab893f5aa8b80ea2720a9e9f53df3ac
Dan Fruehauf <malkodan@gmail.com>
2011-06-22 20:22:24 +1200

    Changed example

--- RRD/munin_hook_example.sh
+++ RRD/munin_hook_example.sh
@@ -4,7 +4,7 @@
 exec >& /dev/null
 
 # change this appropriately
-declare -r MONITIS_RRD_DIR=/usr/share/monitis/RRD
+declare -r MONITIS_RRD_DIR=/usr/share/monitisexchange/Monitis-Linux-Scripts/RRD
 
 # this is usually the default
 declare -r MUNIN_DIR=/var/lib/munin
@@ -14,9 +14,9 @@ main() {
 	# this monitor must exist prior to hooking this script on crontab
 	# the monitor and counters can be easily created using:
 	# cd $MONITIS_RRD_DIR && \
-	#	./monitis_rrd.sh munin add_monitor $MUNIN_DIR localhost memory free active
+	#	./monitis_rrd.sh munin add_monitor $MUNIN_DIR localhost threads threads
 	cd $MONITIS_RRD_DIR && \
-		./monitis_rrd.sh munin update_data_for_monitor $MUNIN_DIR localhost memory free active
+		./monitis_rrd.sh munin update_data_for_monitor $MUNIN_DIR localhost threads threads
 }
 
 main "$@"
