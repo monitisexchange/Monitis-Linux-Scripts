96ca0d5d7403d9c82417ab8f7edb4c53ec6daa6a
Arthur Tumanyan <arthurtumanyan@yahoo.com>
2012-07-03 10:00:44 +0400

    Update master

--- M3/M3Templates.pm
+++ M3/M3Templates.pm
@@ -18,12 +18,12 @@ sub MonitisMonitorManager::_get_PGSQL_DB_NAME {
 
 sub MonitisMonitorManager::_get_API_KEY {
 	# remove croak line after you replace the XXX with your API key
-	return "7A04S52D7FOOS5IC5CPMJEJ0N2";
+	return "xxx";
 }
 
 sub MonitisMonitorManager::_get_SECRET_KEY {
 	# remove croak line after you replace the XXX with your secret key
-	return "7RLFT91MQIBVH024UT6ELH611T";
+	return "xxx";
 }
 
 1;
