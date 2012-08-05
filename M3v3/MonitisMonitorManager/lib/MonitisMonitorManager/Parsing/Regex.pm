ebe09da8bd0cc2d033c58b31317e38c7d3a45bbe
Dan Fruehauf <malkodan@gmail.com>
2012-08-05 13:07:13 +1200

    M3v3: improved parsing of boolean values

--- M3v3/MonitisMonitorManager/lib/MonitisMonitorManager/Parsing/Regex.pm
+++ M3v3/MonitisMonitorManager/lib/MonitisMonitorManager/Parsing/Regex.pm
@@ -36,7 +36,8 @@ sub parse {
 				}
 				carp "Matched '$metric_regex'=>'$data' in '$output_line'";
 				# yield a warning here if it's already in the hash
-				if (defined(${$results}{$metric_name})) {
+				# but don't show it if it's a boolean type :)
+				if (defined(${$results}{$metric_name}) and $metric_type ne "boolean") {
 					carp "Metric '$metric_name' with regex '$metric_regex' was already parsed!!";
 					carp "You should fix your script output to have '$metric_regex' only once in the output";
 				}
@@ -45,8 +46,12 @@ sub parse {
 			} elsif ($metric_type eq "boolean") {
 				# if the data type is a boolean, and we didn't find the result
 				# we were looking for, then it's a 0
-				carp "Matched '$metric_regex'=>'0' in '$output_line'";
-				${$results}{$metric_name} = 0;
+				# however if it was already set (as 1 for instance) we won't reset
+				# it to 0
+				if (not defined(${$results}{$metric_name})) {
+					carp "Matched '$metric_regex'=>'0' in '$output_line'";
+					${$results}{$metric_name} = 0;
+				}
 			}
 		}
 	}
