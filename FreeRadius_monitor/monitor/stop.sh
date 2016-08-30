aaedd537e6b70160009486416c6fad634a251f26
shunanya <simon.hunanyan@monitis.com>
2016-08-30 10:40:24 +0400

    FreeRadius wholly refactored

--- FreeRadius_monitor/monitor/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-pid=`ps -efw | grep -i 'radius_monitor.sh' | grep -v grep | awk '{print $2} ' `
-if test "$pid" ;  then
- kill -KILL $pid
-else
- echo not found radius_monitor.sh
-fi
